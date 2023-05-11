54486a3d5df87a59259c20fa097b939abd79ded5
GG2015 <inquiry.globalgamer2015@gmail.com>
2023-05-11 19:52:19 -0400

    First commit.

---
+++ src/_config.js
@@ -0,0 +1,8 @@
+export var ArkDynmapServer = "http://159.89.228.152";
+export var ArkDynmapApiServer = "http://159.89.228.152:11000/api/players/";
+export let GetPlayerDataTimer = 30; // Seconds
+export var ShowPlayers = true;
+export var ShowBases = true;
+export var ShowTraps = true;
+export var ShowBeds = true;
+export var ActiveServers = [ 'Aberration', 'Extinction', 'Fjordur', 'Genesis2', 'LostIsland', 'ScorchedEarth', 'TheIsland' ];
